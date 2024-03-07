f02e9450dd2345e963851be8aace645318e45930
Mihli_sk06@top-academy.ru <Mihli_sk06@top-academy.ru>
2024-03-07 20:47:23 +0300

    HardChess2 DONE

--- HardChess/main.cpp
+++ HardChess/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-//#define HARD_CHESS_1
-#define HARD_CHESS_2
+#define HARD_CHESS_1
+
 
 void main() {
 	setlocale(LC_ALL, "");
@@ -21,19 +21,4 @@ void main() {
 		}
 	}
 #endif // HAED_CHESS_1
-
-#ifdef HARD_CHESS_2
-	int k = 0, l = 0;
-	for (int i = 0; i < n * n; i++) {
-		for (int j = 0; j < n * n; j++) {
-			if (k < j % n && l) cout << "* ";
-			else {
-				cout << "  ";
-				k++;
-			}
-		}
-		cout << endl;
-	}
-#endif // HARD_CHESS_2
-
 }
